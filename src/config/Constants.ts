6380038bf4d999f33dd33b482c544623da22d8b4
Rafael Kasper <prof.rafaelkasper@gmail.com>
2022-05-16 11:16:57 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: "http://localhost:5000",
   MONGO_CONECTION:
-    "mongodb+srv://rafaelkasper:1FyjCJmD@url-shortener-dio.yo3kl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "colocar aqui a string de conexão",
 };
